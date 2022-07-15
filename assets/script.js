bdfc05b354bb17f2bac9ba28743c7ae36ba824f1
jaysmith13 <janeasmith13@gmail.com>
2022-07-15 17:07:57 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,7 @@ function start(){
     loadSchedule();
     checkingTimeDay();
 }
+//functions for color time blocks
 
 var checkingTimeDay = function(){
     var hourOfDay =timeDay.format("H");
@@ -23,6 +24,38 @@ var checkingTimeDay = function(){
         var index = '#t-'+ i;
         index =index.trim();
         var attr =$(index).attr('atr');
-    }
-}
+
+        if (parseInt(hourOfDay)=== parseInt(attr)){
+            $(index).addClass(bgCurrent);
+            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');
+            if(list.length === 0){
+                $(index).children('.col-10').children('p').text('Current Hour');
+            }
+        }
+        else if(parseInt(attr) < parseInt (hourOfDay)){
+            $(index).addClass(bgBefore);
+            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');}
+        else{
+            $(index).addClass(bgAfter);
+            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');}
+        }
+        }
+//Function for date
+var displayTime = function(){
+    $('#currentDay').text(timeDay.format(timeFormat));
+};
+//event listeners
+$('.col-10').on('click', 'p', function() {
+    var text = $(this)
+    .text()
+    .trim();
+
+    var hourOfDay =timeDay.format('H');
+    var schTime = $(this).parent().parents().attr('atr');
+    var id = "#t-"+schTime;
+    var tempBg = " ";
+
+    if 
+})
+
 
