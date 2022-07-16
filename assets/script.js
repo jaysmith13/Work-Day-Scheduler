46341e27db6a36773a96a1c92785204de68400d0
jaysmith13 <janeasmith13@gmail.com>
2022-07-15 18:43:16 -0600

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -21,39 +21,39 @@ function start(){
 var checkingTimeDay = function(){
     var hourOfDay =timeDay.format("H");
     for(var i = 9; i<18; i++){
-        var index = '#t-'+ i;
+        var index = "#t-"+ i;
         index =index.trim();
-        var attr =$(index).attr('atr');
+        var attr =$(index).attr("atr");
 
         if (parseInt(hourOfDay)=== parseInt(attr)){
             $(index).addClass(bgCurrent);
-            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');
+            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");
             if(list.length === 0){
-                $(index).children('.col-10').children('p').text('Current Hour');
+                $(index).children(".col-10").children("p").text("Current Hour");
             }
         }
         else if(parseInt(attr) < parseInt (hourOfDay)){
             $(index).addClass(bgBefore);
-            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');}
+            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");}
         else{
             $(index).addClass(bgAfter);
-            $(index).children('.business-hour').addClass('text-dark bg-light m-0 p-0 w-100');}
+            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");}
         }
         }
 //Function for date
 var displayTime = function(){
-    $('#currentDay').text(timeDay.format(timeFormat));
+    $("#currentDay").text(timeDay.format(timeFormat));
 };
 //event listeners
-$('.col-10').on('click', 'p', function() {
+$(".col-10").on("click", "p", function() {
     var text = $(this)
     .text()
     .trim();
 
-    var hourOfDay =timeDay.format('H');
-    var schTime = $(this).parent().parents().attr('atr');
+    var hourOfDay =timeDay.format("H");
+    var schTime = $(this).parent().parents().attr("atr");
     var id = "#t-"+schTime;
-    var tempBg = " ";
+    var tempBg = "";
 
     if (parseInt(hourOfDay) === parseInt(schTime)){
         tempBg += bgCurrent;
@@ -64,24 +64,24 @@ $('.col-10').on('click', 'p', function() {
         tempBg += bgAfter;
     }
 
-    var textInput = $('<textarea>')
-    .addClass('form-control text-dark' + tempBg)
+    var textInput = $("<textarea>")
+    .addClass("form-control text-dark" + tempBg)
     .val(text);
 
     $(this).replaceWith(textInput);
-    textInput.trigger('focus');
+    textInput.trigger("focus");
 });
 
 var functionBlur =function (){
-    var text = $('.form-control')
+    var text = $(".form-control")
     .val()
     .trim();
 
- var eventP = $('<p>')
- .addClass('h-100 w-100 mt-4')
+ var eventP = $("<p>")
+ .addClass("h-100 w-100 mt-4")
  .text(text);
 
- $('.form-control').replaceWith(eventP);
+ $(".form-control").replaceWith(eventP);
 }
 
 var loadSchedule = function(){
@@ -89,8 +89,8 @@ var loadSchedule = function(){
         for(var i = 9; i < 18; i++){
             var index = "#t-"+ i;
             index = index.trim();
-            var attr = $(index).attr('atr');
-            var elementP =$(index).children('.col-10').children('p');
+            var attr = $(index).attr("atr");
+            var elementP =$(index).children(".col-10").children("p");
 
             for(j=0; j<list.length; j++)
         {
@@ -102,13 +102,13 @@ var loadSchedule = function(){
     }
 };
 
-$('.btn').on('click', function(event){
+$(".btn").on("click", function(event){
     event.preventDefault();
 
-    var schTime = $(this).attr('atr');
-    var id = '#t-'+schTime;
+    var schTime = $(this).attr("atr");
+    var id = "#t-"+schTime;
     var mostParent = $(this).parents(id);
-    var indexText = mostParent.children('.col-10').children('.form-control');
+    var indexText = mostParent.children(".col-10").children(".form-control");
 
     if(inputText.val())
     {
@@ -118,14 +118,14 @@ $('.btn').on('click', function(event){
         };
 
         list.push(objWorkSchedule);
-        localStorage.setItem('daySch',JSON.stringify(list));
+        localStorage.setItem("daySch",JSON.stringify(list));
         functionBlur();
 
         loadSchedule();
 }
 else
 {
-    alert('You can not save and empty event');
+    alert("You can not save and empty event");
 }
 });
 
