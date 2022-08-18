f6eb0bcd5cdbc0ae07be5c976db1d3210f3a2962
jaysmith13 <janeasmith13@gmail.com>
2022-08-18 17:30:16 -0600

    error update
    fixed row blocks and dates

--- assets/script.js
+++ assets/script.js
@@ -1,132 +1,63 @@
-//background
-var bgCurrent = "bg-danger";
-var bgAfter = "bg-success";
-var bgBefore = "bg-secondary";
-
 //date
-var timeFormat = "dddd, MMMM Do, YYYY";
+var DateFormat = moment().format('dddd, MMM Do YYYY');
+$("#currentDay").html(DateFormat);
 //time
-var timeDay = moment(moment(), "MM/DD/YYYY");
+var TimeOfDay = moment(moment(), "MM/DD/YYYY");
+// Storage for Rows
+$("#hour9 .input").val(localStorage.getItem("hour9"));
+$("#hour10 .input").val(localStorage.getItem("hour10"));
+$("#hour11 .input").val(localStorage.getItem("hour11"));
+$("#hour12 .input").val(localStorage.getItem("hour12"));
+$("#hour13 .input").val(localStorage.getItem("hour13"));
+$("#hour14 .input").val(localStorage.getItem("hour14"));
+$("#hour15 .input").val(localStorage.getItem("hour15"));
+$("#hour16 .input").val(localStorage.getItem("hour16"));
+$("#hour17 .input").val(localStorage.getItem("hour17"));
 
-//LocalStorage Array
-var list = JSON.parse(localStorage.getItem('daySch')) || [];
 
+//Start function
 function start(){
-    displayTime();
+    displayTimeofDay();
     loadSchedule();
-    checkingTimeDay();
+    checkingCurrentDayTime();
 }
-//functions for color time blocks
 
-var checkingTimeDay = function(){
-    var hourOfDay =timeDay.format("H");
-    for(var i = 9; i<18; i++){
-        var index = "#t-"+ i;
-        index =index.trim();
-        var attr =$(index).attr("atr");
+// Save function
+$(document).ready(function () { 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var Input = $(this).siblings(".textInput").val();
+        var time = $(this).parent().attr("id");
 
-        if (parseInt(hourOfDay)=== parseInt(attr)){
-            $(index).addClass(bgCurrent);
-            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");
-            if(list.length === 0){
-                $(index).children(".col-10").children("p").text("Current Hour");
+// Save text locally
+        localStorage.setItem(time, Input);
+    })
+   // Schdeuler functions
+    function DayScheduler() {
+        var timeNow = moment().hour();
+        $(".time-block").each(function () {
+            var RowTime = parseInt($(this).attr("id").split("hour")[1]);
+
+            if (RowTime < timeNow) {
+                $(this).removeClass("later");
+                $(this).removeClass("present");
+                $(this).addClass("past");
             }
-        }
-        else if(parseInt(attr) < parseInt (hourOfDay)){
-            $(index).addClass(bgBefore);
-            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");}
-        else{
-            $(index).addClass(bgAfter);
-            $(index).children(".business-hour").addClass("text-dark bg-light m-0 p-0 w-100");}
-        }
-        }
-//Function for date
-var displayTime = function(){
-    $("#currentDay").text(timeDay.format(timeFormat));
-};
-//event listeners
-$(".col-10").on("click", "p", function() {
-    var text = $(this)
-    .text()
-    .trim();
-
-    var hourOfDay =timeDay.format("H");
-    var schTime = $(this).parent().parents().attr("atr");
-    var id = "#t-"+schTime;
-    var tempBg = "";
-
-    if (parseInt(hourOfDay) === parseInt(schTime)){
-        tempBg += bgCurrent;
-    }
-    else if (parseInt(schTime) < parseInt(hourOfDay)){
-        tempBg += bgBefore;
-    } else {
-        tempBg += bgAfter;
-    }
-
-    var textInput = $("<textarea>")
-    .addClass("form-control text-dark" + tempBg)
-    .val(text);
-
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-});
-
-var functionBlur =function (){
-    var text = $(".form-control")
-    .val()
-    .trim();
-
- var eventP = $("<p>")
- .addClass("h-100 w-100 mt-4")
- .text(text);
-
- $(".form-control").replaceWith(eventP);
-}
-
-var loadSchedule = function(){
-    if (list){
-        for(var i = 9; i < 18; i++){
-            var index = "#t-"+ i;
-            index = index.trim();
-            var attr = $(index).attr("atr");
-            var elementP =$(index).children(".col-10").children("p");
-
-            for(j=0; j<list.length; j++)
-        {
-            if(parseInt(list[j].hourSch) === parseInt(attr)){
-                elementP.text(list[j].txtSch)
-                }
+            else if (RowTime === timeNow) {
+                $(this).removeClass("previous");
+                $(this).removeClass("later");
+                $(this).addClass("current");
             }
-        }
+            else {
+                $(this).removeClass("current");
+                $(this).removeClass("previous");
+                $(this).addClass("later");
+
+            }
+        })
     }
-};
 
-$(".btn").on("click", function(event){
-    event.preventDefault();
-
-    var schTime = $(this).attr("atr");
-    var id = "#t-"+schTime;
-    var mostParent = $(this).parents(id);
-    var indexText = mostParent.children(".col-10").children(".form-control");
-
-    if(inputText.val())
-    {
-        var objWorkSchedule ={
-            txtSch: indputText.val().trim(),
-            hourSch:schTime
-        };
-
-        list.push(objWorkSchedule);
-        localStorage.setItem("daySch",JSON.stringify(list));
-        functionBlur();
-
-        loadSchedule();
-}
-else
-{
-    alert("You can not save and empty event");
-}
-});
-
-start();
+    
+    
+DayScheduler();
+})
